25a726719c7d39ed655f0d2df0b2996a32c3345b
Urs N. <urs.nussbaumer@ict-bz.ch>
2025-09-29 16:13:52 +0200

    Update Program.cs
    Code optimiert und Leerzeilen entfernt.

--- Konsolen Applikationen 1/Aufgabe 5/Aufgabe 5/Aufgabe 5/Program.cs
+++ Konsolen Applikationen 1/Aufgabe 5/Aufgabe 5/Aufgabe 5/Program.cs
@@ -9,16 +9,14 @@ namespace Aufgabe_5
             Console.WriteLine("Wie viele Kilometer möchtest du rennen?");
             int eingabe = Convert.ToInt32(Console.ReadLine());
             int Strecke = 42;
-            
+
             double rundenlänge = 0.4;
             double runden = eingabe / rundenlänge;
-
 
             if (eingabe > Strecke)
             {
                 Console.WriteLine("Das schaffst du nicht");
             }
-
             else
             {
                 Console.WriteLine("Das sind " + runden + " Runden. Bereit für den Lauf?");
@@ -28,51 +26,22 @@ namespace Aufgabe_5
 
                 if (janein > 1)
                     Environment.Exit(0);
-
                 else
                 {
                     int rundenanzahl = 1;
-                    Console.WriteLine("Du läufst jetzt Runde " + rundenanzahl);
-
-
+                    
                     if (rundenanzahl < runden)
                     {
-                        while (rundenanzahl < runden)
+                        while (rundenanzahl <= runden)
                         {
-                            rundenanzahl++;
                             Console.WriteLine("Du läufst jetzt Runde " + rundenanzahl);
+                            rundenanzahl++;
                         }
                         Console.WriteLine("Geschafft! Du hast " + eingabe + " Kilometer geschafft!");
-
-
-
-
-
                     }
-
-
-
-
-
-
-
-
-
                 }
             }
-
-
-
-
-
-
-
         }
-
-
-
-
-
     }
 }
 
